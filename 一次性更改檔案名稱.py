ef6a6137e56b883e8520215a8c80fbd953f88440
redt0cat <47913202+redt0cat@users.noreply.github.com>
2019-06-23 09:01:37 +0800

    06/23

---
+++ 一次性更改檔案名稱.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[174]:
+
+
+import os,shutil
+
+name = 'D:\\高考函授\函授_系統分析設計_張又中'
+print(name)
+list = os.listdir(''+name)
+
+#更改range 確認 要改的檔案在範圍內
+for i in range(40):
+    print(list[i])
+    start = list[i]
+    print(start)
+
+
+# In[176]:
+
+
+#開始名稱 為要命名之路徑檔案
+startname = name+'\\'
+#更改名稱 為所要添加之命名之路徑檔案
+rename = '函授_系統分析設計_張又中 '      
+
+
+for i in range(47):
+    print(list[i])
+    start = startname+list[i]
+    fin = startname+rename+list[i]
+    print(start)
+    print(fin)
+    shutil.move(''+start, ''+fin)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
